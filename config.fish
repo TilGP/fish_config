63b131669bda40106547efc4a265cba00e812602
Til Georg Pockrandt <til.pockrandt@check24.de>
2024-10-14 11:05:38 +0200

    initial commit

---
+++ config.fish
@@ -0,0 +1,27 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go heres
+    alias bp bpython
+    alias vim nvim $1
+    alias :qa exit
+    alias :q exit
+    alias ll "ls -lah $1"
+
+    set LBIN ~/.local/bin/
+end
+
+set PATH ~/.linuxify ~/.cargo/bin /opt/homebrew/opt/llvm/bin /opt/homebrew/opt/llvm@15/bin /opt/homebrew/bin /opt/bin ~/.local/bin ~/go/bin $PATH
+
+source $HOME/.cargo/env.fish
+
+setenv EDITOR nvim
+setenv PAGER most
+setenv GOPRIVATE bitbucket.org
+setenv GOPATH $HOME/go
+setenv SSH_SK_PROVIDER /usr/local/lib/libsk-libfido2.dylib
+setenv DOCKER_CLI_HINTS false
+
+function cdd
+    set dir "$(fd --type d --hidden --follow --exclude .git | fzf +m)" && cd "$dir" || return
+end
+
+test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
