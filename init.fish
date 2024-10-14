c3e8cd7dca9e01883580324ca63261133648cece
Til Georg Pockrandt <til.pockrandt@check24.de>
2024-10-14 13:11:21 +0200

    correctly set prompt and don't load theme

--- init.fish
+++ init.fish
@@ -1,5 +1,6 @@
-fish_config prompt coose nim && fish_config prompt save
-fish_config theme choose "Tomorrow Night Bright" fish_config theme save
+fish_config prompt coose nim
+funcsave fish_prompt
 
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+curl https://raw.githubusercontent.com/TilGP/fish_config/refs/heads/main/fish_plugins >~/.config/fish/fish_plugins
 fisher update
