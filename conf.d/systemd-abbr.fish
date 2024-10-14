065ca237a751e0e5680e153cd48904889869aceb
Til Georg Pockrandt <til.pockrandt@check24.de>
2024-10-14 12:52:29 +0200

    removed conf.d

--- conf.d/systemd-abbr.fish
+++
@@ -1,4 +0,0 @@
-abbr -a -g sc systemctl
-abbr -a -g scu systemctl --user
-abbr -a -g jr journalctl
-abbr -a -g jru journalctl --user
--- init.fish
+++ init.fish
@@ -2,3 +2,4 @@ fish_config prompt coose nim && fish_config prompt save
 fish_config theme choose "Tomorrow Night Bright" fish_config theme save
 
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+fisher update
